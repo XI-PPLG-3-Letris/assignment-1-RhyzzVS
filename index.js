1e47f50222ac7a7aea569cb5153e2ff285e444cd
Rhyzz <ashurametalslug@gmail.com>
2025-10-09 10:34:51 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let tambah = (a, b) => {
+    return a + b
+}
+let kurang = (a, b) => {
+    return a - b
+}
+let kali = (a, b) => {
+    return a * b
+}
+let bagi = (a, b) => {
+    return a / b
+}
+let pangkat = (a, b) => {
+    return a ** b
+}
+let modulus = (a, b) => {
+    return a % b
+}
+
+console.log(tambah(5, 5))
+console.log(kurang(25, 5))
+console.log(kali(5, 15))
+console.log(bagi(5, 10))
+console.log(pangkat(9, 5))
+console.log(modulus(5,2))
